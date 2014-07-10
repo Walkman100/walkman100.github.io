418fdac43cd72b9e074b7537f42e846456d55306
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2014-07-10 18:01:40 +0200

    Rebuild repo step two: add files back
    also fixed one link in the games index, and changed file structure of
    screenshots folder. Next commit will be to do with the screenshots.

---
+++ PHP_MinecraftServerStatus/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Simple Player List by Flabaliki</title>
+<script type="text/javascript" src="refresher.js"></script>
+	<script type="text/javascript">
+		setInterval("phpUpdate('receiver.php')", 10000);
+	</script>
+<style type="text/css">
+	body {
+		background:#f1eade;
+		font-family: Verdana, Arial, Sans-Serif;
+		font-size:13px;
+	}
+	.ip {
+	}
+	.players {
+	}
+	.slots {
+	}
+</style>
+</head>
+<body onload="phpUpdate('receiver.php')">
+<table align="center"><tr><td class="listBody"><span id="a1"></span></td></tr></table>
+</body>
+</html>
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿Walkman <img src="http://walkman100.github.io/favicon.ico" title="Favicon used in my site" alt="Favicon used in my site"><iframe src="http://walkman100.github.io/favicon.ico"></iframe>
+=======
+
+[Walkman's site](http://walkman100.github.com)
+
+The style sheet that is embedded in all pages except the Games folder is [`CSS_JS/main.css`](http://walkman100.github.io/Walkman/CSS_JS/main.css).
+
+Please check out my YouTube Auto Redirector [here](http://walkman100.github.io/Walkman/HTML/YTVL.html) and the Windows version [here](http://github.com/Walkman100/YTVL/releases/latest), screenshots for the Windows version available [here](http://walkman100.github.io/Walkman/HTML/YTVLWindowsScreenshots.html).
