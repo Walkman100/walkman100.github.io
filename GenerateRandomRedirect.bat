c8831932ac32d6faaf64c3227815ee59a0297f2b
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-06-10 04:39:24 +0200

    Fixed Redirect generators, now I've got to fix the existing redirects -_-

--- GenerateRandomRedirect.bat
+++ GenerateRandomRedirect.bat
@@ -4,9 +4,9 @@ color F0
 set redir=%random%
 set /P dest=Redirect destination: 
 
-echo --- > r\%redir%.html
-echo layout: redirect >> r\%redir%.html
-echo dest: %dest% >> r\%redir%.html
-echo --- >> r\%redir%.html
+echo ---> r\%redir%.html
+echo layout: redirect>> r\%redir%.html
+echo dest: %dest%>> r\%redir%.html
+echo --->> r\%redir%.html
 echo Redirect r\%redir% created, goes to: %dest%
 pause
--- GenerateRedirect.bat
+++ GenerateRedirect.bat
@@ -4,7 +4,7 @@ color F0
 SET /P redir=Redirect name: 
 set /P dest=Redirect destination: 
 
-echo --- > %redir%.html
-echo layout: redirect >> %redir%.html
-echo dest: %dest% >> %redir%.html
-echo --- >> %redir%.html
+echo ---> %redir%.html
+echo layout: redirect>> %redir%.html
+echo dest: %dest%>> %redir%.html
+echo --->> %redir%.html
